4c3cd71b8acad2fe44dbd6e908306de73b75bd84
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-25 14:00:13 +0300

    validating props with propsTypes

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,24 @@
-function Button({ children }) {
+import PropTypes from "prop-types";
+function Button({ children, primary, secondary, success, warning, danger }) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  chackVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    //true olan durumların sayısını verir : birden fazla aynı stil için özlellik eklenmemesi için
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary,success,warning,danger can be true"
+      );
+    }
+  },
+};
 
 export default Button;
