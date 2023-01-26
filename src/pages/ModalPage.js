1225697080b9c86243def6cf25336fe3279c0d23
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-26 14:46:44 +0300

    modal close with button

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -12,12 +12,24 @@ function ModalPage() {
     setShowModal(false);
   };
 
+  const actionBar = (
+    <Button onClick={handleClose} primary>
+      Accept
+    </Button>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agrement bla bla </p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={handleClose}></Modal>}
+      {showModal && modal}
     </div>
   );
 }
