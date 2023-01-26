4eb03577eff80c1695c63684a3fa5bf62d84586a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-26 12:34:29 +0300

    link styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,11 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
+
+  const classes = classNames("text-blue-500");
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) return; //ctrl tuşuna basılı tutarak linke tıklarsa sayfa farklı bir sekmede açılır
@@ -12,7 +15,7 @@ function Link({ to, children }) {
   };
 
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
