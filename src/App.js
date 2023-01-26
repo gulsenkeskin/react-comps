dd45c7c3fe378befc29b48584dfea6facf689067
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-26 13:06:30 +0300

    sidebar style updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import Link from "./components/Link";
 import Route from "./components/Route";
+import Sidebar from "./components/Sidebar";
 import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   return (
-    <div>
-      <Link to="/accordion">Go to Accordion</Link>
-      <Link to="/dropdown">Go to Dropdown</Link>
-      <div>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar></Sidebar>
+      <div className="col-span-5">
         <Route path="/accordion">
           <AccordionPage></AccordionPage>
         </Route>
-        <Route path="/dropdown">
+        <Route path="/">
           <DropdownPage></DropdownPage>
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage></ButtonPage>
         </Route>
       </div>
     </div>
