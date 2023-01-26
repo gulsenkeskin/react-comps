b3bcf6c60e4fd1c9b97218a717dac22eb164bc0b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-26 10:24:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React, { useState } from "react";
-import Dropdown from "./components/Dropdown";
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-  return (
-    <Dropdown
-      options={options}
-      value={selection}
-      onChange={handleSelect}
-    ></Dropdown>
-  );
+  return <div></div>;
 }
 
 export default App;
